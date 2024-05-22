3c2fab2213208d1006e4d928b2a7b27eeb194e3f
kimyong <right4570@naver.com>
2024-05-22 20:53:48 +0900

    ex3 완성

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,16 @@ import TextInput from "./components/TextInput";
 function App() {
   // TODO: 로컬 스토리지에서 초기 상태로 사용할 값을 가져오세요. 새로고침 해도 기존 상태를 유지하는 것이 목적입니다.
   // 로컬스토리지에 값이 없을 경우 빈배열[] 로 설정하세요.
-  const [texts, setTexts] = useState([]);
-
+  const [texts, setTexts] = useState(()=>{
+    //useState의 초기값(initialState)을 값만 줘도 되고 아니면 이 처럼 함수형태로해서 return하면 초기값을 달리 설정할수있다.
+    const localData = localStorage.getItem('mylocalData')
+    return localData? JSON.parse(localData):[]
+  });
+  
   useEffect(() => {
     // TODO: 상태가 변경될 때마다 로컬 스토리지에 저장. key 값은 texts 로 합시다.
+    // setTexts(texts);
+    localStorage.setItem("mylocalData", JSON.stringify(texts));
   }, [texts]);
 
   const onAddText = (text) => {
